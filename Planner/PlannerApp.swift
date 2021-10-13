c8c0f78f39a4996b5ce20640f5b4c1f4db6b333c
Mikhail Safir <safir@thenoco.co>
2021-10-13 23:54:40 +0300

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Michael Safir on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
